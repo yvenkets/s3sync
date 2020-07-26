16de2ba6bfe034e6b459275f62cbcec215620154
Venkertraman <yvenkets@gmail.com>
2020-07-26 12:37:22 +0530

    Update synz.sh

--- synz.sh
+++ synz.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 export AWS_CONFIG_FILE="/home/ubuntu/.aws/config"
 
-export AWS_ACCESS_KEY_ID=AKIAY4A3GATPQAHLINRL
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
 aws s3 sync /home/ubuntu/1/ s3://venket/ --acl=public-read --debug
